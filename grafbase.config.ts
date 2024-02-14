12fc27d951192c5951bca45306257446f19824cf
Benjamin Rabier <benjamin+git@rabier.dev>
2024-02-14 20:36:45 +0100

    update

---
+++ grafbase.config.ts
@@ -0,0 +1,28 @@
+import { graph, connector, config } from '@grafbase/sdk'
+
+const g = graph.Standalone()
+
+const petstore = connector.OpenAPI('PetStore', {
+  schema: 'https://petstore3.swagger.io/api/v3/openapi.json',
+  url: 'https://petstore3.swagger.io/api/v3',
+})
+
+g.datasource(petstore)
+
+export default config({
+  graph: g,
+  cache: {
+    rules: [
+      {
+        types: ['Query'],
+        maxAge: 60
+      }
+    ]
+  },
+  auth: {
+    rules: (rules) => {
+      rules.public()
+    }
+  }
+})
+
