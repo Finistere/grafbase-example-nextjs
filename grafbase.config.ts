dfba1f86aa9c4e79e2a08c4390245c7d6100a6ff
Benjamin Rabier <benjamin+git@rabier.dev>
2024-05-20 11:07:21 +0200

    tmp

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,28 +1,33 @@
-import { graph, connector, config } from '@grafbase/sdk'
+import { graph, connector, config } from "@grafbase/sdk";
 
-const g = graph.Standalone()
+const g = graph.Standalone();
 
-const petstore = connector.OpenAPI('PetStore', {
-  schema: 'https://petstore3.swagger.io/api/v3/openapi.json',
-  url: 'https://petstore3.swagger.io/api/v3',
-})
+const petstore = connector.OpenAPI("PetStore", {
+  schema: "https://petstore3.swagger.io/api/v3/openapi.json",
+  url: "https://petstore3.swagger.io/api/v3",
+});
 
-g.datasource(petstore)
+const pg = connector.Postgres("Postgres", {
+  url: "postgresql://neondb_owner:rP3zwFebJsH2@ep-floral-mode-a5i5fub7.us-east-2.aws.neon.tech/neondb?sslmode=require",
+});
+
+g.datasource(pg);
+
+g.datasource(petstore);
 
 export default config({
   graph: g,
   cache: {
     rules: [
       {
-        types: ['Query'],
-        maxAge: 60
-      }
-    ]
+        types: ["Query"],
+        maxAge: 60,
+      },
+    ],
   },
   auth: {
     rules: (rules) => {
-      rules.public()
-    }
-  }
-})
-
+      rules.public();
+    },
+  },
+});
